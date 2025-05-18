e9c1899ec92b9b07b7da8b973e7472134dcb7185
BitQuarky <malelime09liam@gmail.com>
2025-05-18 01:27:00 +0000

    string preliminaries

--- source/repl.d
+++ source/repl.d
@@ -25,7 +25,10 @@ int repl(bool cdebug) {
   auto stack = SList!long();
   int stacksize = 0;
   char[] buf = processAllocator.makeArray!char(64);
-  bool exitloop = false;
+  char[] str = processAllocator.makeArray!char(128);
+  int strpos = 0;
+  int strstart = 0;
+  bool exitloop = false, stringmode = false;
   writeln("starting in interactive mode.\ntype 'exit' to exit the repl.\ntype '?' for general help.\ntype '?' followed by a\nword for documentation.");
   while (!exitloop) {
     write("#> ");
@@ -33,10 +36,42 @@ int repl(bool cdebug) {
     string line = cast(string) buf[0..buf.indexOf("\n")];
     if (cdebug) writeln(line, " length is ", line.length);
     foreach (string s; line.splitter(' ')) {
-      if (isNumeric(s)) {
+      if (s[0] == '"') {
+        stringmode = true;
+        strstart = strpos;
+        int delta = strpos;
+        for (; strpos-delta < s.length-1; strpos++) {
+          str[strpos] = s[strpos-delta+1];
+        }
+        if (s[$-1] == '"') {
+          str[strpos-1] = 0;
+          stringmode = false;
+          stack.insertFront(cast(long) str[strstart..$].ptr);
+        }
+     } else if (isNumeric(s)) {
         stack.insertFront(to!long(s));
         if (cdebug) writeln("number");
         stacksize++;
+      } else if (s[$-1] == '"') {
+        str[strpos++] = ' ';
+        int delta = strpos;
+        writeln("here");
+        for (; strpos-delta < s.length-1; strpos++) {
+          str[strpos] = s[strpos-delta];
+        }
+        str[strpos] = 0;
+        strpos++;
+        stack.insertFront(cast(long) str[strstart..$].ptr);
+        stringmode = false;
+ 
+      } else if (stringmode) {
+        writeln("here3");
+        str[strpos++] = ' ';
+        int delta = strpos;
+        for (; strpos-delta < s.length; strpos++) {
+          str[strpos] = s[strpos-delta];
+        }
+
       } else switch(s) {
         case("exit"):
           exitloop = true;
@@ -50,11 +85,24 @@ int repl(bool cdebug) {
           stack.removeFront();
           stacksize--;
           break;
+        case("print"):
+          if (stack.empty()) {
+            writeln("stack underflow");
+            break;
+          }
+          char* tmp = cast(char*) stack.front();
+          int len = 0;
+          for (; tmp[len]!=0; len++) { }
+          char[] strn = tmp[0..len];
+          writeln(strn);
+          stacksize--;
+          break;
         case("+"):
           if (stacksize < 2) {
             writeln("stack underflow");
             break;
           }
+          stacksize--;
           stack.insertFront(binaryWord(stack, (long x, long y) { return x+y; }));
           break;
         case("-"):
@@ -62,6 +110,7 @@ int repl(bool cdebug) {
             writeln("stack underflow");
             break;
           }
+          stacksize--;
           stack.insertFront(binaryWord(stack, (long x, long y) { return x-y; }));
           break;
         case("*"):
@@ -69,6 +118,7 @@ int repl(bool cdebug) {
             writeln("stack underflow");
             break;
           } 
+          stacksize--;
           stack.insertFront(binaryWord(stack, (long x, long y) { return x*y; }));
           break;
         case("/"):
@@ -76,6 +126,7 @@ int repl(bool cdebug) {
             writeln("stack underflow");
             break;
           } 
+          stacksize--;
           stack.insertFront(binaryWord(stack, (long x, long y) { return x/y; }));
           break;
         case("%"):
@@ -83,6 +134,7 @@ int repl(bool cdebug) {
             writeln("stack underflow");
             break;
           } 
+          stacksize--;
           stack.insertFront(binaryWord(stack, (long x, long y) { return x%y; }));
           break;
         default:
@@ -91,5 +143,7 @@ int repl(bool cdebug) {
       if (exitloop) break;
     }
   }
+  processAllocator.dispose(buf);
+  processAllocator.dispose(str);
   return 0;
 }
